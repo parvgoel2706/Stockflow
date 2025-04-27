27d3860e6555a3af702fd81143d46401ea6d586f
parvgoel2706 <parvgoel2345@gmail.com>
2025-04-28 00:19:36 +0530

    Fix tab selection

--- src/landing_page/pricing/Charges.jsx
+++ src/landing_page/pricing/Charges.jsx
@@ -1,11 +1,19 @@
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet, NavLink, useLocation } from "react-router-dom";
 
 function Charges() {
+  const location = useLocation();
   return (
     <div className="container-lg charges-tab">
       <ul className="nav nav-underline mx-5 border-bottom">
         <li className="nav-item">
-          <NavLink className="nav-link" to="equity">
+          <NavLink
+            className={({ isActive }) =>
+              isActive || location.pathname === "/pricing"
+                ? "nav-link active"
+                : "nav-link"
+            }
+            to="equity"
+          >
             <h4>Equity</h4>
           </NavLink>
         </li>
