ee0ebb1b253eba7821304baf7d287a2259dfeaa1
parvgoel2706 <parvgoel2345@gmail.com>
2025-04-30 16:26:21 +0530

    Interconnent all pages of dashboard

--- dashboard/src/components/LeftTopBar.jsx
+++ dashboard/src/components/LeftTopBar.jsx
@@ -1,23 +1,36 @@
+import { NavLink } from "react-router-dom";
 function LeftTopBar() {
   return (
     <div className="topbar-part border d-flex justify-content-between px-3 align-items-center">
       <img src="/media/images/kiteLogo.webp" alt="" style={{ height: "70%" }} />
       <div className="menu">
-        <span>Dashboard</span>
-        <span>Orders</span>
-        <span>Holdings</span>
-        <span>Positions</span>
-        <span>Funds</span>
-        <span>Apps</span>
+        <NavLink className={({isActive})=>(isActive? "active":"")} to="/">
+          <span>Dashboard</span>
+        </NavLink>
+        <NavLink className={({isActive})=>(isActive? "active":"")} to="/orders">
+          <span>Orders</span>
+        </NavLink>
+        <NavLink className={({isActive})=>(isActive? "active":"")} to="/holdings">
+          <span>Holdings</span>
+        </NavLink>
+        <NavLink className={({isActive})=>(isActive? "active":"")} to="/positions">
+          <span>Positions</span>
+        </NavLink>
+        <NavLink className={({isActive})=>(isActive? "active":"")} to="/funds">
+          <span>Funds</span>
+        </NavLink>
+        <NavLink className={({isActive})=>(isActive? "active":"")} to="/apps">
+          <span>Apps</span>
+        </NavLink>
         &nbsp; &nbsp;
-        <span className="user-logo">
+        <span className="profile">
           &nbsp;
           <img
-            src="media/images/kiteLogo.webp"
+            src="https://ui-avatars.com/api/?name=parv+Goel&rounded=true&&background=d1632c"
             alt=""
-            style={{ height: "50px" }}
+            style={{ height: "35px" }}
           />
-          <span className="userid">USERID</span>
+          <span className="userid">Parv Goel</span>
         </span>
       </div>
     </div>
